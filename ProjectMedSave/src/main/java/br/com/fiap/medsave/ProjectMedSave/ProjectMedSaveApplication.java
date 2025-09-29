3500b11ab76633f67d410a7b3ce193043fe51591
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-29 12:35:59 -0300

    creating project MedSave - Oracle Challenge with SprintBoot + OracleDB

---
+++ ProjectMedSave/src/main/java/br/com/fiap/medsave/ProjectMedSave/ProjectMedSaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medsave.ProjectMedSave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMedSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMedSaveApplication.class, args);
+	}
+
+}
---
+++ ProjectMedSave/src/test/java/br/com/fiap/medsave/ProjectMedSave/ProjectMedSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.medsave.ProjectMedSave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMedSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
