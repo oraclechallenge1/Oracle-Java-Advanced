48e7ea7b8febc465bbf566c5a690990711c52f9a
devpedrosena1 <devpedrosena1@gmail.com>
2026-03-19 23:16:57 -0300

    creating SecurityConfig Filter

---
+++ ProjectMedSave/src/main/java/br/com/fiap/medsave/ProjectMedSave/insfrastructure/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package br.com.fiap.medsave.ProjectMedSave.insfrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
