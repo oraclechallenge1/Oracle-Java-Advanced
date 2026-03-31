4b8e73314c446b5ddf0d833fb2ef838115e46ced
devpedrosena1 <devpedrosena1@gmail.com>
2026-03-31 09:23:05 -0300

    resolving errors and failures to MessageProducer

--- ProjectMedSave/src/main/java/br/com/fiap/medsave/ProjectMedSave/messaging/MessageProducer.java
+++ ProjectMedSave/src/main/java/br/com/fiap/medsave/ProjectMedSave/messaging/MessageProducer.java
@@ -1,5 +1,7 @@
 package br.com.fiap.medsave.ProjectMedSave.messaging;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 @Component
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/messages")
+@Tag(name = "MessageProducer", description = "Endpoints para enviar mensagens para filas e tópicos")
 public class MessageProducer implements CommandLineRunner {
 
     private final JmsTemplate queueJmsTemplate;
@@ -23,11 +26,13 @@ public class MessageProducer implements CommandLineRunner {
     }
 
     @GetMapping
+    @Operation(summary = "Teste de ping", method = "GET")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("Ok");
     }
 
     @PostMapping("/send/queue/{n}")
+    @Operation(summary = "Envia uma quantidade N de mensagens para a fila", method = "POST")
     public ResponseEntity<String> sendQueue(@PathVariable int n) {
         for (int i = 1; i <= n; i++) {
             this.queueJmsTemplate.convertAndSend("demo.queue", "Message " + i);
@@ -36,6 +41,7 @@ public class MessageProducer implements CommandLineRunner {
     }
 
     @PostMapping("/send/topic/{n}")
+    @Operation(summary = "Envia uma quantidade N de tópicos para a fila", method = "POST")
     public ResponseEntity<String> sendTopic(@PathVariable int n) {
         for (int i = 1; i <= n; i++) {
             this.topicJmsTemplate.convertAndSend("demo.topic", "Message Topic " + i);
